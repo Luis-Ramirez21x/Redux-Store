c3a5ed4bb3859adbb2f8c21ca03933225d7ef370
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-02-09 16:36:05 -0500

    useDispatch & useSelector have been imported from react-redux library where needed, app is full converted to redux instaed of react, context

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
+//import { useStoreContext } from '../../utils/GlobalState';
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
@@ -8,10 +8,14 @@ import {
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
-function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+import { useDispatch, useSelector } from 'react-redux';
 
+function CategoryMenu() {
+  //const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
   const { categories } = state;
+  
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
